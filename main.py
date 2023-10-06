5f9669b0102d21441ae257e2653f1199873ef36f
Seungyeoljeon <130431453+Seungyeoljeon@users.noreply.github.com>
2023-10-06 08:58:45 +0000

    다시 로직

--- main.py
+++ main.py
@@ -211,26 +211,25 @@ import streamlit as st
 st.title("💬 모의 면접하기")
 st.caption("🚀 스픽스 모의 면접관입니다.")
 
-if st.button('모의 면접 시작'):
+if st.button('입력된 내용 기반 모의 면접 시작'):
     interveiwer=person + description + "위 내용을 참고해서 전문 면접관 역할을 수행한다. 답변은 한글로 한다. 이제 '안녕하세요. 면접을 시작하겠습니다.'라는 말로 면접을 바로 시작한다."
 
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "user", "content": interveiwer}]
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "user", "content": interveiwer}]
 
-    # 사용자로부터 입력을 받습니다.
-    if user_input := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        
-        # 챗봇의 응답을 생성합니다.
-        response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
-        msg = response.choices[0].message
-        st.session_state.messages.append(msg)
+# 사용자로부터 입력을 받습니다.
+if user_input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    
+    # 챗봇의 응답을 생성합니다.
+    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
 
-        # 메시지 출력
-        for message in st.session_state.messages:
-            st.chat_message(message["role"]).write(message["content"])
-else:
-    None
+    # 메시지 출력
+    for message in st.session_state.messages:
+        st.chat_message(message["role"]).write(message["content"])
+
 
 # if prompt := st.chat_input():
     
