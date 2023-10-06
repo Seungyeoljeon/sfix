22af71356a8fcc699d006c4a13abec0dd59b245b
Seungyeoljeon <130431453+Seungyeoljeon@users.noreply.github.com>
2023-10-06 09:18:55 +0000

    다시수정

--- main.py
+++ main.py
@@ -211,28 +211,31 @@ import streamlit as st
 st.title("💬 모의 면접하기")
 st.caption("🚀 스픽스 모의 면접관입니다.")
 
-def chat_logic():
-    if "interveiwer" not in st.session_state:
-        st.session_state.interveiwer = person + description + "위 내용을 참고해서 전문 면접관 역할을 수행한다. 답변은 한글로 한다. 이제 '안녕하세요. 면접을 시작하겠습니다.'라는 말로 면접을 바로 시작한다."
+# 사용자로부터 person과 description을 입력받는 코드가 필요합니다.
+person = st.text_input("자기 소개")
+description = st.text_input("상황 설명")
 
+if st.button('입력된 내용 기반 모의 면접 시작'):
+    interveiwer = person + description + "위 내용을 참고해서 전문 면접관 역할을 수행한다. 답변은 한글로 한다. 이제 '안녕하세요. 면접을 시작하겠습니다.'라는 말로 면접을 바로 시작한다."
+    st.session_state["messages"] = [{"role": "user", "content": interveiwer}]
+else:
     if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "user", "content": st.session_state.interveiwer}]
+        st.session_state["messages"] = [{"role": "user", "content": "전문 면접관 역할을 수행한다. 답변은 한글로 한다. 이제 '안녕하세요. 면접을 시작하겠습니다.'라는 말로 면접을 바로 시작한다."}]
+
+# 메시지 출력
+for message in st.session_state.get("messages", []):
+    st.chat_message(message["role"]).write(message["content"])
+
+# 사용자로부터 입력을 받습니다.
+if user_input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": user_input})
     
-    if user_input := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        
-        response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
-        msg = response.choices[0].message
-        st.session_state.messages.append(msg)
+    # 챗봇의 응답을 생성합니다.
 
-    for message in st.session_state.messages:
-        st.chat_message(message["role"]).write(message["content"])
-
-# 여기서는 'person'과 'description'이 이미 정의되어 있다고 가정합니다.
-# 예를 들어, 버튼 클릭으로 챗봇 로직을 시작합니다.
-if st.button('챗봇 시작'):
-    chat_logic()
-
+    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state.messages)
+    msg = response.choices[0].message
+    st.session_state.messages.append(msg)
+    
 
 
 # if prompt := st.chat_input():
